0cc4482a43cca4e26914bb383f69c0b5b8d72974
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-07 14:37:40 +0800

    Create navigation section.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,19 @@
-// // Scroll padding
-// const nav = document.querySelector('.nav');
+// Scroll padding
+const nav = document.querySelector('.nav');
 
-// const navHeight = nav.offsetHeight;
-// document.documentElement.style.setProperty(
-//   '--scroll-padding',
-//   navHeight + 'px'
-// );
+const navHeight = nav.offsetHeight;
+document.documentElement.style.setProperty(
+  '--scroll-padding',
+  navHeight + 'px'
+);
 
-// // Menu
+// Menu
 
-// const menuBtn = document.querySelector('.menu-btn');
-// const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
 
-// menuBtn.addEventListener('click', () => {
-//     menuBtn.toggleAttribute('data-open');
-//     menu.classList.toggle('disable');
-//     menu.classList.toggle('enable');
-//   });
+menuBtn.addEventListener('click', () => {
+  menuBtn.toggleAttribute('data-open');
+  menu.classList.toggle('disable');
+  menu.classList.toggle('enable');
+});
