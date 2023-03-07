75f25c29de96e830f3cd27e994aa50700c8f1e12
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-07 14:14:06 +0800

    Add color schemes.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,23 +10,23 @@ module.exports = {
     },
     fontFamily: {
       // heading: [],
-      body: ['Bai Jamjuree', 'sans-serif'],
+      body: ['Inter', 'sans-serif'],
     },
     extend: {
-      // colors: {
-      //   prim: {
-      //     200: '#2c89ac',
-      //     300: '#4496b5',
-      //     400: '#26bba4',
-      //     500: '#116382',
-      //     600: '#0d4a62',
-      //   },
-      //   sec: '#818fff',
-      //   txt: {
-      //     dark: '#212529',
-      //     light: '#666',
-      //   },
-      // },
+      colors: {
+        prim: {
+          200: '#4496b5',
+          300: '#2c89ac',
+          400: '#157ca3',
+          500: '#116382',
+          600: '#0d4a62',
+        },
+        // sec: '#818fff',
+        txt: {
+          dark: '#212529',
+          light: '#666',
+        },
+      },
     },
   },
   plugins: [],
