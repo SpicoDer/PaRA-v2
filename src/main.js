b55e5b464e5862513a04b998795791fbed807459
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-07 14:06:57 +0800

    Starter files.

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// // Scroll padding
+// const nav = document.querySelector('.nav');
+
+// const navHeight = nav.offsetHeight;
+// document.documentElement.style.setProperty(
+//   '--scroll-padding',
+//   navHeight + 'px'
+// );
+
+// // Menu
+
+// const menuBtn = document.querySelector('.menu-btn');
+// const menu = document.querySelector('.menu');
+
+// menuBtn.addEventListener('click', () => {
+//     menuBtn.toggleAttribute('data-open');
+//     menu.classList.toggle('disable');
+//     menu.classList.toggle('enable');
+//   });
