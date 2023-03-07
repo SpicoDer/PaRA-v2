3e93dac3918077cd40297cca59eb1df89ba0ac21
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2023-03-07 18:12:17 +0800

    Add sticky nav bar.

--- src/main.js
+++ src/main.js
@@ -17,3 +17,30 @@ menuBtn.addEventListener('click', () => {
   menu.classList.toggle('disable');
   menu.classList.toggle('enable');
 });
+
+// Scrolling effect for sticky nav bar
+const logo = document.querySelector('.logo');
+
+window.onscroll = function () {
+  let rect = document.querySelector('header').getBoundingClientRect();
+
+  if (rect.width < 996) {
+    nav.classList.add('sticky-nav');
+    nav.classList.add('bg-prim-400');
+    logo.classList.add('text-white');
+
+    if (window.scrollY < 1) {
+      nav.classList.remove('bg-prim-400');
+      logo.classList.remove('text-white');
+    }
+
+    if (window.scrollY > rect.height) {
+      menu.classList.add('disable');
+      menu.classList.remove('enable');
+      menuBtn.removeAttribute('data-open');
+    }
+  } else {
+    // Remove sticky nav on lg screen
+    nav.classList.remove('sticky-nav');
+  }
+};
